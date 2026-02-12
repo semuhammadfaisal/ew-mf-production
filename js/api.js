6f504e55cfaafed118912f3aebfd452de06c13bb
faisalfreelance4-ops <faisalfreelance4@gmail.com>
2026-02-12 17:58:03 +0500

    meta pixel added

--- js/api.js
+++ js/api.js
@@ -1,5 +1,7 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5000/api' 
+  : '/api';
 
 // API Helper Functions
 class API {
